e110853e9946de3705509ccd88e910826897e406
przemyslawcybulski <62156187+przemyslawcybulski@users.noreply.github.com>
2020-12-06 09:11:47 +0100

    Add files via upload

---
+++ arc-path.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ---------------------------------------------------------------------- #
+#    script arc-path.sh 
+#    
+#       author:  przemyslaw.cybulski@comp.com.pl
+#         type:  bash script
+#      version:  1.0
+#
+#    changeLog:
+#                1.0   2020-04-08 cybulprz Initial release
+#
+# ---------------------------------------------------------------------- #
+
+originaldate=$(date '+%Y%m')
+path=/opt/scripts/test-date
+getdate=$(cat $path | grep 3].folder=)
+getdate2=${getdate#*/*/*/*/}
+getdate3=${getdate2%/*}
+
+if [ $originaldate = $getdate3 ]  
+then
+    echo ""
+else
+    sed -i "s/${getdate3}/${originaldate}/g" $path
+    /etc/init.d/arc_nazwa_uslugi restart
+fi
+exit 0
